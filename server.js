81b3a8d982cff309a83eff289112630491f01ec4
cerebrium <nicholas.m.shankland@gmail.com>
2019-12-16 15:50:25 -0800

    fixed one line for deployment

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
+app.use(express.static(__dirname + '/client/build'));
 app.use(helmet())
 
 mongoose.connect(process.env.MONGODB_URI)
