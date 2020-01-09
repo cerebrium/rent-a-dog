4dc1e33234a2305dc93bf8e20be3cc30c898f617
cerebrium <nicholas.m.shankland@gmail.com>
2020-01-08 18:20:17 -0800

    added base image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,6 +26,7 @@ const App = () => {
       axios.post('/auth/signup', {
         name: response.profileObj.name, 
         email: response.profileObj.email, 
+        photo: 'https://res.cloudinary.com/sross9845/image/upload/v1578533522/petapi/kdelzzpcccqszxbgnjr1.jpg'
       }).then(res => {
         setUser(res.data)
         console.log(res.data)
